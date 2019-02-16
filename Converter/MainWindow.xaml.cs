2049bfb6352b1b98c1b65938dd6f37c44888dc20
Beau DeYoung <beaudeyoung@gmail.com>
2019-02-15 20:51:56 -0500

    final push

--- Converter/MainWindow.xaml.cs
+++ Converter/MainWindow.xaml.cs
@@ -29,18 +29,61 @@ namespace Converter
         {
             double feet;
 
-            feet = double.Parse(TextBox_Meters.Text) * 3.28;
+            if (validateMetersInputs())
+            {
+                feet = double.Parse(TextBox_Meters.Text) * 3.28;
 
-            TextBox_Feet.Text = feet.ToString();
+                TextBox_Feet.Text = feet.ToString();
+            }
+            
         }
 
         private void Button_Meters_Click(object sender, RoutedEventArgs e)
         {
             double meters;
 
-            meters = double.Parse(TextBox_Feet.Text) / 3.28;
+            if (validateFeetInputs())
+            {
+                meters = double.Parse(TextBox_Feet.Text) / 3.28;
 
-            TextBox_Meters.Text = meters.ToString();
+                TextBox_Meters.Text = meters.ToString();
+            }
+
+        }
+
+        private bool validateFeetInputs()
+        {
+            bool validInputs = true;
+
+            if (!double.TryParse(TextBox_Feet.Text, out double feet))
+            {
+                MessageBox.Show("Please enter numbers for the feild");
+                validInputs = false;
+                ResetInputs();
+            }
+
+            return validInputs;
+        }
+
+        private bool validateMetersInputs()
+        {
+            bool validInputs = true;
+
+            if (!double.TryParse(TextBox_Meters.Text, out double meters))
+            {
+                MessageBox.Show("Please enter numbers for the feild");
+                validInputs = false;
+                ResetInputs();
+            }
+
+            return validInputs;
+        }
+
+        private void ResetInputs()
+        {
+            TextBox_Feet.Text = "";
+            TextBox_Meters.Text = "";
+            TextBox_Feet.Focus();
         }
     }
 }
